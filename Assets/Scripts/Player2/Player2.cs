acc9c7da6612f813069264e3e88b0d35dd0d1a38
TranPhuongU <bukkiyama@gmail.com>
2024-11-22 22:07:14 +0700

    init commit

---
+++ Assets/Scripts/Player2/Player2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirection))] 
+public class Player2 : MonoBehaviour
+{
+    TouchingDirection touchingDirection;
+    public bool _isFacingRight = true;
+    public float speed;
+    //public float runSpeed;
+    Animator animator;
+    Rigidbody2D rb;
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    Vector2 moveInput;
+    public float jumpForce;
+
+    public float CurrentSpeed
+    {
+        get
+        {
+            {
+                if (IsMoving && !touchingDirection.IsOnWall)
+                {
+                    return speed;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+      
+    // animation di chuyển
+    public bool IsMoving { get 
+        { 
+            return _isMoving;
+        }
+        private set 
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+       }
+
+    //quay hướng mặt player
+    public bool IsFacingRight { get 
+        {
+            return _isFacingRight;
+        } private set 
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1,1);
+            }
+            _isFacingRight = value;
+        } 
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDirection = GetComponent<TouchingDirection>();
+    }
+    
+    private void FixedUpdate()
+    {
+        // Di chuyển của player
+        rb.velocity = new Vector2(moveInput.x * CurrentSpeed, rb.velocity.y);
+
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+    //Quay hướng mặt player 
+    public void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            //Face is right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            //Face is left
+            IsFacingRight = false;
+        }
+    }
+    
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //todo check if alive as well
+        if (context.started && touchingDirection.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Player2/TouchingDirection.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchingDirection : MonoBehaviour
+{
+    public ContactFilter2D castFilter;
+    CapsuleCollider2D touchingCol;
+
+    public float groundDistance = 0.05f;
+    public float wallDistance = 0.2f;
+    public float ceilingDistance = 0.05f;
+
+    RaycastHit2D[] groundHits = new RaycastHit2D[5];
+    RaycastHit2D[] wallHits = new RaycastHit2D[5];
+    RaycastHit2D[] ceilingHits = new RaycastHit2D[5];
+
+    Animator animator;
+
+    [SerializeField]
+    private bool _isOnWall;
+
+    //Kiểm tra xem có ở dưới đất hay không
+    public bool IsOnWall
+    {
+        get
+        {
+            return _isOnWall;
+        }
+        private set
+        {
+            _isOnWall = value;
+            animator.SetBool("isOnWall", value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isOnCeiling;
+
+    //Kiểm tra xem có ở dưới đất hay không
+    public bool IsOnCeiling
+    {
+        get
+        {
+            return _isOnCeiling;
+        }
+        private set
+        {
+            _isOnCeiling = value;
+            animator.SetBool("isOnCeiling", value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isGrounded;
+
+    //Kiểm tra xem có ở dưới đất hay không
+    public bool IsGrounded { get 
+        {
+            return _isGrounded;
+        } private set 
+        {
+            _isGrounded = value;
+            animator.SetBool("isGrounded", value);
+        } }
+
+    public Vector2 WallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();  
+        touchingCol = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // VA CHẠM VỚI GROUND
+        IsGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
+        //VA CHẠM VỚI TƯỜNG
+        IsOnWall = touchingCol.Cast(WallCheckDirection, castFilter, wallHits,wallDistance) > 0;
+        // VA CHẠM VỚI TRẦN NHÀ
+        IsOnCeiling = touchingCol.Cast(Vector2.up, castFilter, ceilingHits, ceilingDistance) > 0;
+    }
+}
